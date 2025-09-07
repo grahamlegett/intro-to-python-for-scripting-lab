cbf4835e1032a8dd28c5b9e8d2b9fd77fd1ed2f4
Lee G <graham.legett@gmail.com>
2025-09-07 17:44:12 -0400

    commit message

---
+++ hello_python.py
@@ -0,0 +1,9 @@
+# value to store username
+# Asks for user name input
+fname = input("Enter your first name:")
+lname = input("Enter your last name: ")
+# Prints "Hello World!" to the console
+print("Hello World!")
+# Prints user first and last name
+print(fname, lname)
+print("Welcome to the Python Zone!!")
