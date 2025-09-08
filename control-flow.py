3710725c8c700067f630483c0b5d07e3ba981f8d
Lee G <graham.legett@gmail.com>
2025-09-07 20:16:52 -0400

    commit message

--- control-flow.py
+++ control-flow.py
@@ -38,3 +38,47 @@ def check_letter():
 
 # Call the function
 check_letter()
+
+# Problem 2: Old enough to vote?
+#
+# Write a Python function named `check_voting_eligibility` that determines if a
+# user is old enough to vote.
+# Fill in the logic to perform the eligibility check inside the function.
+#
+# Function Details:
+# - Prompt the user to input their age: "Please enter your age: "
+# - Validate the input to ensure the age is a possible value (no negative 
+#   numbers).
+# - Determine if the user is eligible to vote. Set a variable for the voting
+#   age.
+# - Print a message indicating whether the user is eligible to vote based on the
+#   entered age.
+#
+# Hints:
+# - Use the `input()` function to capture the user's age.
+# - Use `int()` to convert the input to an integer. Ensure that you handle any 
+#   conversion errors gracefully. You may need to look up how to do this online.
+# - Use a conditional statement to check if the age meets the minimum voting age
+#   requirement.
+
+def check_voting_eligibility():
+    # Your control flow logic goes here
+    voting_age = 18
+    age = input("Please enter your age: ")
+    try:
+        entered_age = int(age)
+    except ValueError as e:
+        print(f"Error: {e}"); 
+
+    if entered_age > 0 and entered_age <= 17:
+        print("You are NOT eligible to vote!")
+    elif entered_age > 0 and entered_age >= voting_age:
+        print("You ARE eligible to vote!")
+    else: 
+        print("INVALID ENTRY!!")  
+              
+
+
+
+# Call the function
+check_voting_eligibility()
